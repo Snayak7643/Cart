2fb375855086062c2efa7b3221baa6bc9d31b090
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-11 16:48:37 +0530

    round off the total

--- src/Reducers/functions.tsx
+++ src/Reducers/functions.tsx
@@ -1,14 +1,18 @@
 import { State } from "../type";
 
 export const increaseAmount = (state: State, price: number) => {
-  return { ...state, amount: state.amount + 1, total: state.total + price };
+  return {
+    ...state,
+    amount: state.amount + 1,
+    total: Math.round((state.total + price) * 100) / 100,
+  };
 };
 
 export const decreaseAmount = (state: State, amount: number, price: number) => {
   return {
     ...state,
     amount: state.amount - amount,
-    total: Math.round(state.total - amount * price),
+    total: Math.round((state.total - amount * price) * 100) / 100,
   };
 };
 
