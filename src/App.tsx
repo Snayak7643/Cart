a0c832cd6dcd2c49d08b796806ddae5809ffc983
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-11 12:52:55 +0530

    useEffect for calculateTotal done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import ProductList from "./Components/ProductList";
 import { reducer } from "./Reducers/reducer";
 import CartContext from "./Contexts/CartContext";
 import { initialCartState } from "./Constants/InitialCartState";
+import productsData from "./data";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialCartState);
+
+  useEffect(() => {
+    let total = 0;
+    const calculateTotal = () => {
+      productsData.forEach((product) => {
+        total = total + product.price;
+      });
+    };
+    calculateTotal();
+    dispatch({
+      type: "set_initial",
+      payload: { id: 0, amount: 4, price: total },
+    });
+    console.log(total);
+  }, []);
 
   return (
     <CartContext.Provider value={{ state, dispatch }}>
